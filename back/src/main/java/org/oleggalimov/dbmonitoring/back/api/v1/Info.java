c0056c7f9ce4a2b6069d36e69bda45a64d38e28b
oleg <o.d.galimov@yandex.ru>
2019-09-21 15:48:24 +0500

    + test suite

--- back/src/main/java/org/oleggalimov/dbmonitoring/back/api/v1/Info.java
+++ back/src/main/java/org/oleggalimov/dbmonitoring/back/api/v1/Info.java
@@ -3,17 +3,15 @@ package org.oleggalimov.dbmonitoring.back.api.v1;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
 @Path("/v1")
-public class Info extends Application {
+public class Info {
 
     @GET
     @Path("/status")
-    @Produces({"application/json; charset=UTF-8"})
+    @Produces({"application/json"})
     public Response getStatus() {
-        System.out.println("Запрос на сервлет");
         return Response.ok().entity("Service online").build();
     }
 }
